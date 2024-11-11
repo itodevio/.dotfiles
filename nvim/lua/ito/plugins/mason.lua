756b4baf309aaf6daf8ba7fc6f01791a34015437
Italo Assunção <italoaa99@gmail.com>
2024-11-11 12:01:06 -0300

    adding deno lsp

--- nvim/lua/ito/plugins/mason.lua
+++ nvim/lua/ito/plugins/mason.lua
@@ -2,12 +2,19 @@ local lspconfig = require('lspconfig')
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 local util = require('lspconfig/util')
 
+-- Adding Deno support to ts files
+vim.g.markdown_fenced_languages = {
+"ts=typescript"
+}
+lspconfig.denols.setup{}
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
     'lua_ls',
     'eslint',
     'gopls',
+    'denols',
     'html',
     'cssls',
     'tailwindcss',
